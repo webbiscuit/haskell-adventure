5bc8eebe326ee71437db79977b3b968f48f9d9d8
Daniel <daniel@webbiscuit.co.uk>
2019-08-15 22:40:45 +0100

    Remove some things we don't need right now

--- src/Gamebook.hs
+++ src/Gamebook.hs
@@ -5,8 +5,6 @@ module Gamebook
     Section (..)
   ) where
 
-import Data.List
-import System.IO
 import Text.Regex.PCRE
 import Data.String.Utils
 
@@ -22,24 +20,24 @@ data Choice = Choice {
   destination :: SectionNumber
 } deriving (Show, Eq)
 
-findChoices :: SectionNumber -> [Choice] -> [Choice]
-findChoices n = filter (\x -> (source x) == n)
+-- findChoices :: SectionNumber -> [Choice] -> [Choice]
+-- findChoices n = filter (\x -> (source x) == n)
 
-findSection :: SectionNumber -> [Section] -> Maybe Section
-findSection n = find (\x -> (sectionNumber x) == n) 
+-- findSection :: SectionNumber -> [Section] -> Maybe Section
+-- findSection n = find (\x -> (sectionNumber x) == n) 
 
-formatChoice :: Choice -> String
-formatChoice c = (choiceText c) ++ " - " ++ show (destination c)
+-- formatChoice :: Choice -> String
+-- formatChoice c = (choiceText c) ++ " - " ++ show (destination c)
 
-displayChoices :: [Choice] -> String
-displayChoices = intercalate "\n" . map formatChoice
+-- displayChoices :: [Choice] -> String
+-- displayChoices = intercalate "\n" . map formatChoice
 
-displaySection :: Maybe Section -> [Choice] -> String
-displaySection Nothing _ = ""
-displaySection (Just s) xs = sectionText s ++ "\n" ++ displayChoices (findChoices (sectionNumber s) xs) 
+-- displaySection :: Maybe Section -> [Choice] -> String
+-- displaySection Nothing _ = ""
+-- displaySection (Just s) xs = sectionText s ++ "\n" ++ displayChoices (findChoices (sectionNumber s) xs) 
 
-play :: [Section] -> [Choice] -> SectionNumber -> IO()
-play sections choices n = putStr $ displaySection (findSection n sections) choices ++ "\n"
+-- play :: [Section] -> [Choice] -> SectionNumber -> IO()
+-- play sections choices n = putStr $ displaySection (findSection n sections) choices ++ "\n"
 
 
 bookParseRegex :: String
@@ -78,11 +76,11 @@ parseTextIntoSections =  matchesToSections . parseText
 loadBook :: IO String
 loadBook = readFile "./app/books/fridge.txt"
 
-test = [["1\n\nAfter a restless night full of sleepless dreams, you awake to find yourself trapped in a fridge. \n\nTurn to 2.\n\n","1","\n\nAfter a restless night full of sleepless dreams, you awake to find yourself trapped in a fridge. \n\nTurn to 2.\n\n"],["2\n\nThere's some food all around.\n\nTry to open the door. Turn to 3.\nEat some food. Turn to 4.\nGive up and die. Turn to 5.\n\n","2","\n\nThere's some food all around.\n\nTry to open the door. Turn to 3.\nEat some food. Turn to 4.\nGive up and die. Turn to 5.\n\n"],["3\n\nThe door does not seem to open.\n\nTry to open the door. Turn to 3.\nEat some food. Turn to 4.\nGive up and die. Turn to 5.\n\n","3","\n\nThe door does not seem to open.\n\nTry to open the door. Turn to 3.\nEat some food. Turn to 4.\nGive up and die. Turn to 5.\n\n"],["4\n\nThere is so much food here.\nYou eat so much food you die.\n\nThe End\n\n","4","\n\nThere is so much food here.\nYou eat so much food you die.\n\nThe End\n\n"],["5\n\nYou have died.\n\nThe End\n\n","5","\n\nYou have died.\n\nThe End\n\n"],["6\n\n","6","\n\n"],["7\n\n","7","\n\n"],["8\n\n","8","\n\n"],["9\n\n","9","\n\n"],["10\n\n","10","\n\n"],["11\n\n","11","\n\n"],["12\n\n","12","\n\n"],["13\n\n","13","\n\n"],["14\n\n","14","\n\n"]]
-test2 = do
-  book <- loadBook
-  let sections = parseTextIntoSections book
-  let choices = parseSectionsIntoChoices sections
-  play sections choices 2
-test3 = Section 1 "I am a section.\n\nSomething happens. Turn to 2.\n\nSomething else. Turn to 3."
-test4 = "Some ‘ thing"
+-- test = [["1\n\nAfter a restless night full of sleepless dreams, you awake to find yourself trapped in a fridge. \n\nTurn to 2.\n\n","1","\n\nAfter a restless night full of sleepless dreams, you awake to find yourself trapped in a fridge. \n\nTurn to 2.\n\n"],["2\n\nThere's some food all around.\n\nTry to open the door. Turn to 3.\nEat some food. Turn to 4.\nGive up and die. Turn to 5.\n\n","2","\n\nThere's some food all around.\n\nTry to open the door. Turn to 3.\nEat some food. Turn to 4.\nGive up and die. Turn to 5.\n\n"],["3\n\nThe door does not seem to open.\n\nTry to open the door. Turn to 3.\nEat some food. Turn to 4.\nGive up and die. Turn to 5.\n\n","3","\n\nThe door does not seem to open.\n\nTry to open the door. Turn to 3.\nEat some food. Turn to 4.\nGive up and die. Turn to 5.\n\n"],["4\n\nThere is so much food here.\nYou eat so much food you die.\n\nThe End\n\n","4","\n\nThere is so much food here.\nYou eat so much food you die.\n\nThe End\n\n"],["5\n\nYou have died.\n\nThe End\n\n","5","\n\nYou have died.\n\nThe End\n\n"],["6\n\n","6","\n\n"],["7\n\n","7","\n\n"],["8\n\n","8","\n\n"],["9\n\n","9","\n\n"],["10\n\n","10","\n\n"],["11\n\n","11","\n\n"],["12\n\n","12","\n\n"],["13\n\n","13","\n\n"],["14\n\n","14","\n\n"]]
+-- test2 = do
+--   book <- loadBook
+--   let sections = parseTextIntoSections book
+--   let choices = parseSectionsIntoChoices sections
+--   play sections choices 2
+-- test3 = Section 1 "I am a section.\n\nSomething happens. Turn to 2.\n\nSomething else. Turn to 3."
+-- test4 = "Some ‘ thing"
