2a365b08dcf987018e922e43c68e015674858627
Daniel <daniel@webbiscuit.co.uk>
2019-08-23 23:01:11 +0100

    Fix warnings

--- app/Main.hs
+++ app/Main.hs
@@ -6,14 +6,22 @@ import System.Exit
 
 main :: IO ()
 main = getArgs >>= parse >>= putStr . parser
-    where parser input = show (parseTextIntoBook input)
+    where parser = show . parseTextIntoBook
 
+parse :: [String] -> IO String
 parse ["-h"] = usage   >> exit
 parse ["-v"] = version >> exit
 parse []     = getContents
 parse fs     = concat `fmap` mapM readFile fs
 
+usage :: IO ()
 usage   = putStrLn "Usage: haskelladventure [-vh] [file ..]"
+
+version :: IO ()
 version = putStrLn "Haskell Adventure 0.1"
+
+exit :: IO a
 exit    = exitWith ExitSuccess
+
+die :: IO a
 die     = exitWith (ExitFailure 1)
